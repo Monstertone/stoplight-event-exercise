640efd0c3969ad3591fa3df2aa6833dda7122708
Thomas Gabauer <tgabauer@gmail.com>
2018-08-16 10:31:25 -0700

    added the function for the slow button and go button along with the light function for both

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 (function() {
   'use strict';
-  let stopbutton = document.getElementById('stopButton');
-  let slowbutton = document.getElementById('slowButton');
-  let gobutton = document.getElementById('goButton');
+  let stopButton = document.getElementById('stopButton');
+  let slowButton = document.getElementById('slowButton');
+  let goButton = document.getElementById('goButton');
 
   let stopLight = document.getElementById('stopLight');
   let slowLight = document.getElementById('slowLight');
@@ -24,12 +24,58 @@
   function mouseOverStop (){
     console.log('Entered Stop Button!');
   }
-  function mouseOutStop ()
-  {
+  function mouseOutStop (){
     console.log('Left Stop Button');
 
   }
-  stopButton.addEventListener('mouseover', 'mouseOutStop');
-  stopButton.addEventListener('mouseout', 'mouseOutStop');
+  stopButton.addEventListener('mouseover', mouseOverStop);
+  stopButton.addEventListener('mouseout', mouseOutStop);
   // YOUR CODE HERE
+
+
+
+  // SLOWBUTTON
+
+   slowButton.addEventListener('click', function(){
+     if (slowLight.style.backgroundColor === 'yellow') {
+       slowLight.style.backgroundColor = 'black'
+       console.log('Slow light is off!');
+     } else if (slowLight.style.backgroundColor !== 'yellow') {
+       slowLight.style.backgroundColor = 'yellow';
+       console.log('Slow light is On!');
+     }
+
+   })
+
+   function mouseOverSlow() {
+     console.log('Entered Slow Button')
+   }
+   function mouseOutSlow() {
+     console.log('Left Slow Button')
+   }
+
+   slowButton.addEventListener('mouseover' , mouseOverSlow)
+   slowButton.addEventListener('mouseout' , mouseOutSlow)
+
+//GO Button
+
+  goButton.addEventListener('click' , function(){
+    if (goLight.style.backgroundColor === 'green'){
+      goLight.style.backgroundColor = 'black'
+      console.log('Go light is off!');
+    } else if (goLight.style.backgroundColor !== 'green'){
+      goLight.style.backgroundColor = 'green';
+      console.log('Go light is on!')
+    }
+  })
+
+  function mouseOverGo() {
+    console.log('Entered Go Button')
+  }
+  function mouseOutGo() {
+    console.log('Left Go Button')
+  }
+
+  goButton.addEventListener('mouseover' , mouseOverGo)
+  goButton.addEventListener('mouseout' , mouseOutGo)
 })();
